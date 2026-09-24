79249c7921d1d752fccd484f544ac89222d9da59
ahmad <ii4253476@gmail.com>
2026-09-24 21:22:54 +0700

    fix kecil appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('local') || env('FORCE_HTTPS', false)) {
-            URL::forScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
